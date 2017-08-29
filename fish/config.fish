f8800af0571e4214d6ad3ad554974377b73c5973
Ryan Morehart <ryan@moreharts.com>
2017-08-29 10:49:10 -0600

    Add pip-installed user binaries to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,14 @@
 # Add Go
 set -gx GOPATH $HOME/go
-set -gx PATH /usr/local/go/bin/ $PATH
+if test -d /usr/local/go/bin >/dev/null
+    set -gx PATH /usr/local/go/bin/ $PATH
+end
+if test -d ~/go/bin >/dev/null
+    set -gx PATH ~/go/bin $PATH
+end
 
 # Add personal binaries
-set -gx PATH ~/go/bin ~/bin $PATH
+set -gx PATH ~/bin ~/.local/bin $PATH
 
 # Python/venv
 eval (/usr/bin/python3 -m virtualfish auto_activation)
