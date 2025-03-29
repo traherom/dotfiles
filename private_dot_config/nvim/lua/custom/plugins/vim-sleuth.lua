68f3e3de507b254c4583d50e32cda3a070accb75
Ryan Morehart <ryan@moreharts.com>
2025-03-29 06:48:30 -0600

    Moved to Kickstart-based nvim config

---
+++ private_dot_config/nvim/lua/custom/plugins/vim-sleuth.lua
@@ -0,0 +1,3 @@
+return {
+    'pope/vim-sleuth',
+}
--- private_dot_config/nvim/lua/plugins/whichkey.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "folke/which-key.nvim",
-    -- keys = {
-    --   { "<leader>e", "<cmd>Neotree reveal_force_cwd dir=%:p:h<CR>", desc = "Reveal current file in Neotree" },
-    -- },
-  },
-}
